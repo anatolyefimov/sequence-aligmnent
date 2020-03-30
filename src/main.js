5cf9cc319b232efa25b93b90d0e33adcfcb2a46a
anatolyefimov <efimo.tolik@gmail.com>
2020-03-30 20:07:29 +0300

    Rewrite algorithm

--- src/main.js
+++ src/main.js
@@ -27,59 +27,57 @@ $input.addEventListener('submit', function(event){
     let n = s.length;
     let m = t.length;
 
-    let f = new Array(n + 1);
+    let editDistance = new Array(n + 1);
     for (let i = 0; i <= n; ++i) {
-        f[i] = new Array(m + 1);
+        editDistance[i] = new Array(m + 1);
     }
 
     for (let i = 0; i <= n; ++i) {
-        f[i][0] = -2*i;
+        editDistance[i][0] = i;
     }
 
     for (let j = 0; j <= m; ++j) {
-        f[0][j] = -2*j;
+
+        editDistance[0][j] = j;
     }
 
-    const similarity = (a, b) => a === b ? 2 : -1;
+    const difference = (a, b) => a === b ? 0 : 1;
 
     for (let i = 1; i <= n; ++i) {
         for (let j = 1; j <= m; ++j) {
-            f[i][j] = Math.max(
-                f[i- 1][j - 1] + similarity(s[i - 1], t[j - 1]),
-                f[i - 1][j] - 2,
-                f[i][j - 1] - 2,
-            );
+            if (s[i - 1] === t[j - 1]) {
+                editDistance[i][j] = editDistance[i - 1][j - 1];
+            } else {
+                editDistance[i][j] = Math.min(
+                    editDistance[i - 1][j],
+                    editDistance[i][j - 1],
+                    editDistance[i - 1][j - 1],
+                ) + 1;
+            }
         }
     }
 
-    let i = n, j = m;
-    let editDistance = 0;
     let a = '', b = '';
+    let i = n, j = m;
     while (i > 0 || j > 0 ) {
-        if (i > 0 && j > 0 && f[i][j] === f[i - 1][j - 1] + similarity(s[i - 1], t[j - 1])) {
+        if (i > 0 && j > 0 && editDistance[i][j] === editDistance[i - 1][j - 1] + difference(s[i - 1], t[j - 1])) {
             a = s[i - 1] + a;
             b = t[j - 1] + b;
             --i;
             --j;
-        } else if (i > 0 && f[i][j] === f[i - 1][j] - 2) {
+        } else if (i > 0 && editDistance[i][j] === editDistance[i - 1][j] + 1) {
             a = s[i - 1] + a;
             b = '-' + b;
             --i;
-        } else if (j > 0 && f[i][j] === f[i][j - 1] - 2) {
+        } else if (j > 0 && editDistance[i][j] === editDistance[i][j - 1] + 1) {
             a = '-' + a;
             b = t[j - 1] + b;
             --j;
         }
     }
-
-    for (let i = 0; i < a.length; ++i) {
-        if (a[i] !== b[i]) {
-            ++editDistance;
-        }
-    }
     let $output = document.querySelector('.output');
     let [$editDistance, $firstSequnce, $seconSequence] = $output.children;
-    $editDistance.textContent = editDistance;
+    $editDistance.textContent = editDistance[n][m];
     $firstSequnce.textContent = a;
     $seconSequence.textContent = b;
 });
